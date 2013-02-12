a03e210a840277d6f31bb8b5592305f0f291c4cf
Jeremy Reichman <jaharmi@jaharmi.com>
2013-02-11 23:35:36 -0500

    Initial revision.

---
+++ xprotect_meta_plist_java_minimum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import os
+import Foundation
+import sys
+
+xprotect_meta_plist = "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/XProtect.meta.plist"
+plist_nsdata, error_msg = Foundation.NSData.dataWithContentsOfFile_options_error_(xprotect_meta_plist, Foundation.NSUncachedRead, None)
+xprotect_meta, plist_format, error_msg = Foundation.NSPropertyListSerialization.propertyListFromData_mutabilityOption_format_errorDescription_(plist_nsdata, Foundation.NSPropertyListMutableContainers, None, None)
+
+extension_attribute = xprotect_meta['PlugInBlacklist']['10']['com.oracle.java.JavaAppletPlugin']['MinimumPlugInBundleVersion']
+print('%s' % extension_attribute)
