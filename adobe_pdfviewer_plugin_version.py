5e95076aaf2405c081ef0edf7ad4a29df8e52d96
Jeremy Reichman <jaharmi@jaharmi.com>
2013-03-13 21:05:30 -0400

    Added Adobe PDF Viewer plugin extension attribute.

---
+++ adobe_pdfviewer_plugin_version.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+# Copyright (c) 2013 by Jeremy Reichman <jaharmi@jaharmi.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import os.path
+import Foundation
+
+pdfviewer_plugin_path = "/Library/Internet Plug-Ins/AdobePDFViewer.plugin"
+
+# Determine if the plugin path exists and is not a broken symlink
+if os.path.exists(pdfviewer_plugin_path):
+    pdfviewer_plist = os.path.join(pdfviewer_plugin_path, "Contents/Info.plist")
+    try:
+        plist_nsdata, error_msg = Foundation.NSData.dataWithContentsOfFile_options_error_(pdfviewer_plist, Foundation.NSUncachedRead, None)
+        plist_data, plist_format, error_msg = Foundation.NSPropertyListSerialization.propertyListFromData_mutabilityOption_format_errorDescription_(plist_nsdata, Foundation.NSPropertyListMutableContainers, None, None)
+        extension_attribute = plist_data['CFBundleVersion']
+    except KeyError:
+        extension_attribute = "Unknown"
+
+# Plugin path does not exist or is a broken symlink    
+else:
+    extension_attribute = "Missing"
+
+extension_attribute_result = "<result>%s</result>" % extension_attribute
+print(extension_attribute_result)
